d555caf9c9bdf721fba619e9fcc1fe7b263c8b6d
Abigail Vanessa <abigailduran137@gmail.com>
2025-12-08 16:53:54 -0300

    Cambios temporales en api.php antes de pasar a main

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProvinciaController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Rutas de Provincia
+Route::get('/provincias', [ProvinciaController::class, 'index']);
+Route::post('/provincias', [ProvinciaController::class, 'store']);
+Route::get('/provincias/{id}', [ProvinciaController::class, 'show']);
+Route::put('/provincias/{id}', [ProvinciaController::class, 'update']);
+Route::delete('/provincias/{id}', [ProvinciaController::class, 'destroy']);
