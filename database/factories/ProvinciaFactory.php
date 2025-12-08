c441a4a8d4b1dfdf189c94b382a68ff40ca19613
Abigail Vanessa <abigailduran137@gmail.com>
2025-12-08 17:12:08 -0300

    Guardo cambios antes de cambiar de rama

--- database/factories/ProvinciaFactory.php
+++ database/factories/ProvinciaFactory.php
@@ -13,8 +13,6 @@ class ProvinciaFactory extends Factory
     {
         return [
             'nombre' => $this->faker->unique()->state() . ' ' . $this->faker->numberBetween(1, 100),
-            'codigo' => $this->faker->unique()->stateAbbr(),
-            'region' => $this->faker->randomElement(['Norte', 'Sur', 'Centro', 'Este', 'Oeste', 'Pampeana', 'Patagonia', 'Cuyo', 'NOA', 'NEA']),
         ];
     }
 }
