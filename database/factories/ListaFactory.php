9804463f37a1d1773c7b3de05af6ba0f99aab6ab
Abigail Vanessa <abigailduran137@gmail.com>
2025-12-08 02:25:41 -0300

    Configuro ListaFactory para tests unitarios

--- database/factories/ListaFactory.php
+++ database/factories/ListaFactory.php
@@ -1,23 +1,25 @@
 <?php
 
+// Configuro ListaFactory para tests unitarios
+
 namespace Database\Factories;
 
+use App\Models\Lista;
+use App\Models\Provincia;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Lista>
- */
 class ListaFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    protected $model = Lista::class;
+
+    public function definition(): array
     {
         return [
-            //
+            'nombre' => $this->faker->company() . ' - Lista ' . $this->faker->numberBetween(1, 100),
+            'alianza' => $this->faker->optional()->company(),
+            'cargoDiputado' => $this->faker->randomElement(['true', 'false', '1', '0']),
+            'cargoSenador' => $this->faker->randomElement(['true', 'false', '1', '0']),
+            'idProvincia' => Provincia::factory(),
         ];
     }
 }
